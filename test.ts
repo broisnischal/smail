6b323db609217cf93f018a40d03736f8247620fc
broisnischal <neeswebservice@gmail.com>
2025-06-17 01:23:46 +0545

    feat:ps

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "localhost",
-  port: 25,
+  port: 2525,
   secure: false,
 });
 
@@ -13,7 +13,7 @@ async function testEmail() {
 
     const info = await transporter.sendMail({
       from: '"Test Sender" <test@example.com>',
-      to: "codewithnws@snehaa.store",
+      to: "cybnischal@snehaa.store",
       subject: "Test Email from Node.js",
       text: "This is a test email sent to your SMTP server",
       html: "<b>This is a test email</b> sent to your SMTP server",
