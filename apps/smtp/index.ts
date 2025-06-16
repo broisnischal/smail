1a1884bf6667649f5cadf6ebd10b35f37c87a350
broisnischal <neeswebservice@gmail.com>
2025-06-17 02:07:23 +0545

    ftea

--- apps/smtp/index.ts
+++ apps/smtp/index.ts
@@ -2,6 +2,8 @@ import { SMTPServer } from "smtp-server";
 import { simpleParser } from "mailparser";
 import * as nodemailer from "nodemailer";
 import { PrismaClient } from "../../shared/generated/prisma";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 const prisma = new PrismaClient();
 
@@ -9,8 +11,8 @@ const prisma = new PrismaClient();
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // Use SSL
+    port: 587,
+    secure: false, // Use SSL
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_APP_PASSWORD,
